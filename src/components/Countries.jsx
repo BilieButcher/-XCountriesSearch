a56cace467398b6bcc5868dac457c3616c1b07ec
Swapnil Sahu <sahu.swapnil496@gmail.com>
2025-02-12 01:02:52 +0530

    xcountries2

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -30,16 +30,16 @@ const Countries = () => {
     },[]);
 
     return(
-        <div className="countryCard">
-            <div className="countryCard" style={{height:"50px", border:"1px solid black", padding:"5px", paddingTop:"0px", marginBottom:"10px", background:"rgb(248,248,248)"}}>
+        <div >
+            <div  style={{height:"50px", border:"1px solid black", padding:"5px", paddingTop:"0px", marginBottom:"10px", background:"rgb(248,248,248)"}}>
            <input onChange={(e) => {setSearch(e.target.value)}} type="text" placeholder="Search for countries..." style={{width:"400px", height:"40px", borderRadius:"5px", margin:"4px"}}></input>
            </div>
-            <div className="countryCard" style={{display:"flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center"}}>
+            <div  style={{display:"flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center"}}>
            {country.filter((index) => {
             return search.toLowerCase() === '' ? index : index.common.toLowerCase().includes(search)
            }).map((index) => {
             return(
-                <Card className="countryCard"
+                <Card 
                 flag={index.png}
                 name={index.common}
                 />
